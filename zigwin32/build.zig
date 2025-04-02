847f6f8442a77a034285dc23792985e41a8b33d9
Nagata Parama Aptana <61454123+lifers@users.noreply.github.com>
2025-04-02 17:27:31 -0400

    Delete zigwin32 directory

--- zigwin32/build.zig
+++
@@ -1,8 +0,0 @@
-const builtin = @import("builtin");
-const std = @import("std");
-
-pub fn build(b: *std.Build) void {
-    _ = b.addModule("win32", .{
-        .root_source_file = b.path("win32.zig"),
-    });
-}
